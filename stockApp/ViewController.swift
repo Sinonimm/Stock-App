0a40bfb92d17cc8a44a19fb5066c37cd445e649a
Sinonimm <shchemelev.ilia@gmail.com>
2022-02-19 23:21:16 +0300

    working app (fixed wrong link)

--- stockApp/ViewController.swift
+++ stockApp/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBOutlet weak var companyPickerView: UIPickerView!
     @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companySymbolLabel: UILabel!
+    @IBOutlet weak var PriceLabel: UILabel!
+    @IBOutlet weak var PriceChangeLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    private let companies: [String: String] = ["Apple":"AAPL",
-                                               "Microsoft":"MSFT",
-                                               "Google":"GOOG",
-                                               "Amazon":"AMZN",
-                                               "Facebook":"FB",]
+    private let companies: [String: String] = ["Apple":"aapl",
+                                               "Microsoft":"msft",
+                                               "Google":"goog",
+                                               "Amazon":"amzn",
+                                               "Facebook":"fb",]
     
     
     
@@ -42,13 +45,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             
             guard
                 let json = jsonObject as? [String: Any],
-                let companyName = json["companyName"] as? String
+                let companyName = json["companyName"] as? String,
+                let companySymbol = json["symbol"] as? String,
+                let price = json["latestPrice"] as? Double,
+                let priceChange = json["change"] as? Double
             else{
                 print("! Invalid JSON format")
                 return
             }
             DispatchQueue.main.async {
-                self.displayStockInfo(companyName: companyName)
+                self.displayStockInfo(companyName: companyName, symbol: companySymbol, price: price, priceChange: priceChange)
             }
 
         }catch{
@@ -56,14 +62,17 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
     }
     
-    private func displayStockInfo(companyName: String){
+    private func displayStockInfo(companyName: String, symbol: String, price: Double, priceChange: Double){
         self.activityIndicator.stopAnimating()
         self.companyNameLabel.text = companyName
+        self.companySymbolLabel.text = symbol
+        self.PriceLabel.text = "\(price)"
+        self.PriceChangeLabel.text = "\(priceChange)"
     }
     
     
     private func requestQuote(for symbol: String){
-        let url = URL(string:"https://cloud.iexapis.com/stable/stock/\(symbol)/quote")!
+        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?&token=pk_4d16f034d7a7446a98901a1257caee09")!
         
         let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
             guard
@@ -71,7 +80,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                 (response as? HTTPURLResponse)?.statusCode == 200,
                 let data = data
             else{
-                print("! Network error")
+                print(error)
                 return
             }
             self.parseQuote(data: data)
@@ -91,6 +100,9 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     private func requestQuoteUpdate(){
         self.activityIndicator.startAnimating()
         self.companyNameLabel.text = "-"
+        self.companySymbolLabel.text = "-"
+        self.PriceLabel.text = "-"
+        self.PriceChangeLabel.text = "-"
         
         let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
         let selctedSymbol = Array(self.companies.values)[selectedRow]
