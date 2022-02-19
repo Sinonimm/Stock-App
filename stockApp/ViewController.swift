66c4d35119ee4d3af92a756ec5e7b4bc6556b9da
Sinonimm <shchemelev.ilia@gmail.com>
2022-02-19 20:37:09 +0300

    Picker and Dictionaries added

--- stockApp/ViewController.swift
+++ stockApp/ViewController.swift
@@ -7,16 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
 
     @IBOutlet weak var companyPickerView: UIPickerView!
     @IBOutlet weak var companyNameLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    private let companies: [String: String] = ["Apple":"AAPL",
+                                               "Microsoft":"MSFT",
+                                               "Google":"GOOG",
+                                               "Amazon":"AMZN",
+                                               "Facebook":"FB",]
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.companies.keys.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(self.companies.keys)[row]
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.companyNameLabel.text = "Tinkoff FinTech Company Russia Federatin"
+        self.companyNameLabel.text = "Tinkoff"
+        
+        self.companyPickerView.dataSource = self
+        self.companyPickerView.delegate = self
     }
 
 
