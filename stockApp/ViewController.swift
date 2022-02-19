5907675e1d43184cd44be738dffc205186847b5d
Sinonimm <shchemelev.ilia@gmail.com>
2022-02-19 21:51:34 +0300

    Parsing and JSON

--- stockApp/ViewController.swift
+++ stockApp/ViewController.swift
@@ -35,13 +35,79 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     
+    
+    private func parseQuote(data: Data){
+        do{
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let json = jsonObject as? [String: Any],
+                let companyName = json["companyName"] as? String
+            else{
+                print("! Invalid JSON format")
+                return
+            }
+            DispatchQueue.main.async {
+                self.displayStockInfo(companyName: companyName)
+            }
+
+        }catch{
+            print("! JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    private func displayStockInfo(companyName: String){
+        self.activityIndicator.stopAnimating()
+        self.companyNameLabel.text = companyName
+    }
+    
+    
+    private func requestQuote(for symbol: String){
+        let url = URL(string:"https://cloud.iexapis.com/stable/stock/\(symbol)/quote")!
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else{
+                print("! Network error")
+                return
+            }
+            self.parseQuote(data: data)
+        }
+        
+        dataTask.resume()
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.activityIndicator.startAnimating()
+        
+        let selectedSymbol = Array(self.companies.values)[row]
+        self.requestQuote(for: selectedSymbol)
+    }
+    
+    private func requestQuoteUpdate(){
+        self.activityIndicator.startAnimating()
+        
+        let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
+        let selctedSymbol = Array(self.companies.values)[selectedRow]
+        self.requestQuote(for: selctedSymbol)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.companyNameLabel.text = "Tinkoff"
-        
         self.companyPickerView.dataSource = self
         self.companyPickerView.delegate = self
+        
+        self.activityIndicator.hidesWhenStopped = true
+        
+        self.requestQuoteUpdate()
     }
 
 
